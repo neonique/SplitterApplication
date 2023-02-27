88e10722810b2915ce4bb7e9e71870cc283fb387
neonique <nickgarczorz@gmail.com>
2023-02-27 13:01:02 +0100

    ADD Domain Tests

---
+++ Splitter/src/test/java/de/propra/splitter/domain/TestDomain.java
@@ -0,0 +1,15 @@
+package de.propra.splitter.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+public class TestDomain {
+
+  @Test()
+  @DisplayName("Create Transaction using Users")
+  void test_01(){
+
+  }
+
+}
