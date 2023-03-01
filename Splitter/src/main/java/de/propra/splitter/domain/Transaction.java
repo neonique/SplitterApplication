1b8cfe19a8cb058805153be77bf29b024e95ecdd
elli0404 <mreynier02@gmail.com>
2023-03-01 11:40:19 +0100

    ADD checking if user is part of transaction

--- Splitter/src/main/java/de/propra/splitter/domain/Transaction.java
+++ Splitter/src/main/java/de/propra/splitter/domain/Transaction.java
@@ -5,6 +5,18 @@ import javax.money.MonetaryAmount;
 import org.javamoney.moneta.Money;
 import org.springframework.lang.NonNull;
 
-public record Transaction(@NonNull User sponsor,@NonNull Set<User> Participants, @NonNull Money money) {
+public record Transaction(@NonNull User sponsor,@NonNull Set<User> beggers, @NonNull Money money) {
+
+  public boolean isBegger(User user) {
+    return beggers.contains(user);
+  }
+
+  public boolean isSponsor(User user) {
+    return user.equals(sponsor);
+  }
+
+  public boolean isParticipant (User user) {
+    return isSponsor(user)||isBegger(user);
+  }
 
 }
