bc84c27921844ef5e76377d55d3a4912f20c3165
Moaz-hal <m.halimeh@muhammads-MacBook-Pro.local>
2023-03-07 10:16:16 +0100

    ADD OAuth2Authentication

--- Splitter/src/main/java/de/propra/splitter/web/SplitterController.java
+++ Splitter/src/main/java/de/propra/splitter/web/SplitterController.java
@@ -1,19 +1,34 @@
 package de.propra.splitter.web;
 
-import de.propra.splitter.domain.Nutzer;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class SplitterController {
+
+
 
   public SplitterController(){
     System.out.println("abc");
   }
   @GetMapping("/")
-  public String startSeite(){
+  public String startSeite(OAuth2AuthenticationToken auth, Model m){
 
-    return "startSeite" ;
+    return "anmelden";
   }
+  @PostMapping("/")
+  public String post(){
 
+    return "anmelden";
+
+}
+
+/*@GetMapping("/alleGruppen")
+public String alleGruppen(Model m,){
+
+  }*/
 }
