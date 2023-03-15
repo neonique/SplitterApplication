6f16fdfa64cb674f42aa45820e365f1f34ea6c29
Moaz-hal <m.halimeh@muhammads-MacBook-Pro.local>
2023-03-15 11:05:05 +0100

    ADD db tables erstellt

--- Splitter/src/main/resources/db.migration/V1_init.sql
+++ Splitter/src/main/resources/db.migration/V1_init.sql
@@ -4,3 +4,23 @@ CREATE TABLE IF NOT EXISTS gruppe_data
     gruppenname VARCHAR(300),
     geschlossen BOOLEAN
 );
+CREATE TABLE IF NOT EXISTS transaktion_data
+(
+    id INT  PRIMARY KEY,
+    betrag DOUBLE NOT NULL,
+    sponsor VARCHAR(300),
+    beschreibung VARCHAR(300),
+    FOREIGN KEY (id) REFERENCES gruppe_data(id)
+);
+CREATE TABLE IF NOT EXISTS gruppe_nutzer_relation
+(
+    id INT  PRIMARY KEY,
+    nutzername VARCHAR(300),
+    FOREIGN KEY (id) REFERENCES gruppe_data(id)
+);
+CREATE TABLE IF NOT EXISTS gruppe_transaktion_realtion
+(
+    nutzername VARCHAR(300)
+    FOREIGN KEY (id) REFERENCES transaktion_data(id)
+);
+
