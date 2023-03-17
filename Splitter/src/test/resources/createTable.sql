ac9e12fda30df9d7a56ed3261bf4d6a1cec4ff86
elli0404 <mreynier02@gmail.com>
2023-03-17 10:21:27 +0100

    FIX db tables

--- Splitter/src/test/resources/createTable.sql
+++ Splitter/src/test/resources/createTable.sql
@@ -1,29 +1,30 @@
 CREATE TABLE IF NOT EXISTS gruppe_data
 (
-    gruppenid VARCHAR(36) PRIMARY KEY,
+    gruppenid VARCHAR(36),
+    gruppenIntId INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     gruppenname VARCHAR(300),
     geschlossen BOOLEAN
-    );
+);
 CREATE TABLE IF NOT EXISTS transaktion_data
 (
-    transaktionid SERIAL PRIMARY KEY,
+    transaktionid INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     betrag DOUBLE NOT NULL,
     sponsor VARCHAR(300),
     beschreibung VARCHAR(300),
     gruppenid VARCHAR(36),
     FOREIGN KEY (gruppenid) REFERENCES gruppe_data(gruppenid)
-    );
+);
 CREATE TABLE IF NOT EXISTS gruppe_nutzer_relation
 (
-    id SERIAL PRIMARY KEY,
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     gruppenid VARCHAR(36),
     nutzername VARCHAR(300),
     FOREIGN KEY (gruppenid) REFERENCES gruppe_data(gruppenid)
-    );
+);
 CREATE TABLE IF NOT EXISTS nutzer_transaktion_relation
 (
-    id SERIAL PRIMARY KEY,
+    id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nutzername VARCHAR(300),
-    transaktionid SERIAL,
+    transaktionid INTEGER,
     FOREIGN KEY (transaktionid) REFERENCES transaktion_data(transaktionid)
-    );
+);
